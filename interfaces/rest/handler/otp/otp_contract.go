cc8d87083acf3dde4e95905cba406ff214ffe991
Yoga Mandayu Dian Grahadi <YOG196@PID-PRODUCTENGINEER-YOG196.local>
2024-10-31 18:26:18 +0700

    update workflow

--- interfaces/rest/handler/otp/otp_contract.go
+++ interfaces/rest/handler/otp/otp_contract.go
@@ -1,17 +1,23 @@
 package otp
 
-import "github.com/yogamandayu/ohmytp/domain/entity"
+import (
+	"github.com/yogamandayu/ohmytp/domain/entity"
+	"strings"
+)
 
 // RequestOtpRequestContract is request otp request contract.
 type RequestOtpRequestContract struct {
 	RouteType  string `json:"route_type"`
 	RouteValue string `json:"route_value"`
+	Purpose    string `json:"purpose"`
+	Length     int    `json:"length"`
+	Expiration int    `json:"expiration"`
 }
 
 // TransformToOtpEntity is to transform to otp entity.
 func (r RequestOtpRequestContract) TransformToOtpEntity() entity.Otp {
 	return entity.Otp{
-		RouteType: r.RouteType,
+		RouteType: strings.ToUpper(r.RouteType),
 	}
 }
 
