221a490f9394208f4e15d04b2a13b9058d71085d
yogamandayu <yoga.grahadi@gmail.com>
2024-11-03 23:48:01 +0700

    add comment

--- interfaces/rest/response/failed_response.go
+++ interfaces/rest/response/failed_response.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 )
 
+// FailedResponse is a struct to hold http failed response data.
 type FailedResponse struct {
 	Error          string `json:"error"`
 	Code           string `json:"code"`
@@ -12,6 +13,7 @@ type FailedResponse struct {
 	HTTPStatusCode int    `json:"-"`
 }
 
+// NewHTTPFailedResponse is a constructor.
 func NewHTTPFailedResponse(errCode string, err error, message string) *FailedResponse {
 	return &FailedResponse{
 		Code:    errCode,
@@ -20,11 +22,13 @@ func NewHTTPFailedResponse(errCode string, err error, message string) *FailedRes
 	}
 }
 
+// WithStatusCode is to set http status code to failed response.
 func (f *FailedResponse) WithStatusCode(code int) *FailedResponse {
 	f.HTTPStatusCode = code
 	return f
 }
 
+// AsJSON is to set success response as JSON.
 func (f *FailedResponse) AsJSON(w http.ResponseWriter) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(f.HTTPStatusCode)
--- interfaces/rest/response/success_response.go
+++ interfaces/rest/response/success_response.go
@@ -5,12 +5,14 @@ import (
 	"net/http"
 )
 
+// SuccessResponse is a struct to hold http success response data.
 type SuccessResponse struct {
 	Data           interface{} `json:"data"`
 	Message        string      `json:"message"`
 	HTTPStatusCode int         `json:"-"`
 }
 
+// NewHTTPSuccessResponse is a constructor.
 func NewHTTPSuccessResponse(data interface{}, message string) *SuccessResponse {
 	return &SuccessResponse{
 		Data:    data,
@@ -18,11 +20,13 @@ func NewHTTPSuccessResponse(data interface{}, message string) *SuccessResponse {
 	}
 }
 
+// WithStatusCode is to set http status code to success response.
 func (s *SuccessResponse) WithStatusCode(code int) *SuccessResponse {
 	s.HTTPStatusCode = code
 	return s
 }
 
+// AsJSON is to set success response as JSON.
 func (s *SuccessResponse) AsJSON(w http.ResponseWriter) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(s.HTTPStatusCode)
