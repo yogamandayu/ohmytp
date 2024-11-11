ab14a2fff6449f41652134eabdf4f83496a71e9c
Yoga Mandayu Dian Grahadi <YOG196@PID-PRODUCTENGINEER-YOG196.local>
2024-11-11 17:38:23 +0700

    add throttle third threshold test

--- internal/throttle/throttle_test.go
+++ internal/throttle/throttle_test.go
@@ -118,4 +118,56 @@ func TestThrottle(t *testing.T) {
 		}
 	})
 
+	t.Run("Try to run process 15 times to test third threshold (3-5-7)", func(t *testing.T) {
+		th := throttle.NewThrottle(testSuite.App.Redis, "example_process", uuid.NewString()).SetThresholds([]throttle.Threshold{
+			{
+				MaxAttempt:      3,
+				WaitingDuration: 1 * time.Second,
+			},
+			{
+				MaxAttempt:      5,
+				WaitingDuration: 1 * time.Second,
+			},
+			{
+				MaxAttempt:      7,
+				WaitingDuration: 24 * 60 * time.Minute,
+			},
+		})
+		for i := 0; i < 15; i++ {
+			ok, err := th.IsAllowed(context.Background())
+			if i == 2 || i == 7 {
+				time.Sleep(1 * time.Second)
+			}
+			require.NoError(t, err)
+			assert.True(t, ok)
+		}
+	})
+
+	t.Run("Try to run process 9 times to test third threshold (3-5-7)", func(t *testing.T) {
+		th := throttle.NewThrottle(testSuite.App.Redis, "example_process", uuid.NewString()).SetThresholds([]throttle.Threshold{
+			{
+				MaxAttempt:      3,
+				WaitingDuration: 1 * time.Second,
+			},
+			{
+				MaxAttempt:      5,
+				WaitingDuration: 60 * time.Minute,
+			},
+			{
+				MaxAttempt:      7,
+				WaitingDuration: 24 * 60 * time.Minute,
+			},
+		})
+		for i := 0; i < 15; i++ {
+			ok, err := th.IsAllowed(context.Background())
+			if i == 2 || i == 7 {
+				time.Sleep(1 * time.Second)
+			}
+			require.NoError(t, err)
+			if i == 15 {
+				assert.False(t, ok)
+			}
+		}
+	})
+
 }
