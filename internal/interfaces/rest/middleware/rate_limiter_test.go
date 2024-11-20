12b2039d69a8bfd1ef882d9f20053b997fa0191c
Yoga Mandayu Dian Grahadi <YOG196@PID-PRODUCTENGINEER-YOG196.local>
2024-11-20 19:27:20 +0700

    update rate limiter middleware

--- internal/interfaces/rest/middleware/rate_limiter_test.go
+++ internal/interfaces/rest/middleware/rate_limiter_test.go
@@ -1,11 +1,10 @@
 package middleware_test
 
 import (
-	"testing"
-
 	"github.com/stretchr/testify/assert"
 	"github.com/yogamandayu/ohmytp/internal/interfaces/rest/middleware"
 	"github.com/yogamandayu/ohmytp/tests"
+	"testing"
 )
 
 func TestRateLimiter(t *testing.T) {
--- internal/interfaces/rest/route/otp_route.go
+++ internal/interfaces/rest/route/otp_route.go
@@ -1,21 +1,17 @@
 package route
 
 import (
-	"net/http"
-	"time"
-
 	"github.com/yogamandayu/ohmytp/internal/app"
 	"github.com/yogamandayu/ohmytp/internal/interfaces/rest/handler/otp"
 	"github.com/yogamandayu/ohmytp/internal/interfaces/rest/middleware"
+	"net/http"
 )
 
 // OTPRoute is route for all otp handler.
 func OTPRoute(mux *http.ServeMux, app *app.App) {
 	otpHandler := otp.NewHandler(app)
 
-	defaultRateLimit := middleware.NewRateLimit(app).
-		WithFixedWindow(1000, 1*time.Minute).
-		LimitByIPAddress()
+	defaultRateLimit := middleware.NewRateLimit(app)
 
 	mux.HandleFunc("POST /api/v1/otp/request", defaultRateLimit.SetProcessName("otp:v1:request_otp").Apply(
 		http.HandlerFunc(otpHandler.Request),
